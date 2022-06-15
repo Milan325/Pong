094327893dc5703adee6f546d3b155a1ba755e62
Milan325 <106485944+Milan325@users.noreply.github.com>
2022-06-15 18:10:27 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,41 +3,45 @@ from paddle import Paddle, middle_line
 from ball import Ball
 from scoreboard import Scoreboard
 
-screen = Screen()
-screen.bgcolor("black")
-screen.setup(width=800, height=600)
-screen.title("Pong")
-middle_line()
-screen.tracer(1)
+#### GUI ENGINE ####
+if __name__ == "__main__":
+    screen = Screen()
+    screen.bgcolor("black")
+    screen.setup(width=800, height=600)
+    screen.title("Pong")
+    middle_line()
+    screen.tracer(1)
 
-r_paddle = Paddle((350, 0))
-l_paddle = Paddle((-350, 0))
-ball = Ball()
-scoreboard = Scoreboard()
+    r_paddle = Paddle((350, 0))
+    l_paddle = Paddle((-350, 0))
+    ball = Ball()
+    scoreboard = Scoreboard()
+    
+    # defining key input for players
+    screen.listen()
+    screen.onkey(r_paddle.go_up, "Up")
+    screen.onkey(r_paddle.go_down, "Down")
+    screen.onkey(l_paddle.go_up, "w")
+    screen.onkey(l_paddle.go_down, "s")
 
-screen.listen()
-screen.onkey(r_paddle.go_up, "Up")
-screen.onkey(r_paddle.go_down, "Down")
-screen.onkey(l_paddle.go_up, "w")
-screen.onkey(l_paddle.go_down, "s")
+    # game engine
+    game_is_on = True
+    while game_is_on:
+        screen.update()
+        ball.move()
 
-game_is_on = True
-while game_is_on:
-    screen.update()
-    ball.move()
+        if ball.ycor() > 280 or ball.ycor() < -280:
+            ball.bounce_y()
 
-    if ball.ycor() > 280 or ball.ycor() < -280:
-        ball.bounce_y()
+        if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+            ball.bounce_x()
 
-    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
-        ball.bounce_x()
+        if ball.xcor() > 380:
+            ball.reset_position()
+            scoreboard.l_point()
 
-    if ball.xcor() > 380:
-        ball.reset_position()
-        scoreboard.l_point()
+        if ball.xcor() < -380:
+            ball.reset_position()
+            scoreboard.r_point()
 
-    if ball.xcor() < -380:
-        ball.reset_position()
-        scoreboard.r_point()
-
-screen.exitonclick()
+    screen.exitonclick()
