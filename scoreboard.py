cf36ba89a0a87e8e3e2a878a120bd6aef400acea
Milan325 <106485944+Milan325@users.noreply.github.com>
2022-06-15 18:05:28 +0200

    Update scoreboard.py

--- scoreboard.py
+++ scoreboard.py
@@ -2,6 +2,7 @@ from turtle import Turtle
 
 
 class Scoreboard(Turtle):
+    """the class creates scoreboard at the top of the screen using Turtle object"""
 
     def __init__(self):
         super().__init__()
@@ -13,6 +14,7 @@ class Scoreboard(Turtle):
         self.update_scoreboard()
 
     def update_scoreboard(self):
+        """the module writes player's score defined as l_score and r_score attributes"""
         self.clear()
         self.goto(-100, 200)
         self.write(self.l_score, align="center", font=("Courier", 80, "normal"))
@@ -20,9 +22,11 @@ class Scoreboard(Turtle):
         self.write(self.r_score, align="center", font=("Courier", 80, "normal"))
 
     def l_point(self):
+        """the module increases right player's score by one and updates scoreboard"""
         self.l_score += 1
         self.update_scoreboard()
 
     def r_point(self):
+        """the module increases left player's score by one and updates scoreboard"""
         self.r_score += 1
         self.update_scoreboard()
