3ab60dbb5859dddec11c914ef913874c245b768b
Milan325 <106485944+Milan325@users.noreply.github.com>
2022-06-15 18:01:58 +0200

    Update paddle.py

--- paddle.py
+++ paddle.py
@@ -2,6 +2,7 @@ from turtle import Turtle
 
 
 class Paddle(Turtle):
+    """this class creates Paddle object out of Turtle object"""
 
     def __init__(self, position):
         super().__init__()
@@ -12,15 +13,18 @@ class Paddle(Turtle):
         self.goto(position)
 
     def go_up(self):
+        """the module takes Paddle object's position on y axis and increases it by 20px"""
         new_y = self.ycor() + 20
         self.goto(self.xcor(), new_y)
 
     def go_down(self):
+        """the module takes Paddle object's position on y axis and decreases it by 20px"""
         new_y = self.ycor() - 20
         self.goto(self.xcor(), new_y)
 
 
 def middle_line(center=(0, 300)):
+    """the function creates line on the screen using Turtle object and pendown() method"""
     line = Turtle()
     line.hideturtle()
     line.color("yellow")
